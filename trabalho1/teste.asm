9cb0472f88fbcea59a66fc897239fb6ecf5ece5e
João Bolsson <joaovictorbolsson@gmail.com>
2018-10-12 20:10:53 -0300

    commit inicial

---
+++ trabalho1/teste.asm
@@ -0,0 +1,5 @@
+# exemplo de instruções R, I e J
+l0:
+add  $t0, $t1, $t2
+addi $t1, $t2, 234
+j l0
---
+++ trabalho1/trabalho.s
@@ -0,0 +1,13 @@
+#**********************************************
+#
+#	Trabalho 1 - Organização de Computadores
+#
+# Descrição: Programa que faz a leitura de um arquivo binário contendo instruções em 
+# linguagem de máquina e imprime no console a correspondência de cada linha em linguagem Assembly
+#
+# Autores: 
+# 	João Víctor Bolsson Marques (jvmarques@inf.ufsm.br)
+# Assembler: MARS
+#
+#
+#**********************************************
